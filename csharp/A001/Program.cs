f308799aab8bf22605441778148a442eca71e082
FlashBBBBBack <1092011683@qq.com>
2021-02-26 15:30:37 +0800

    第一次提交

---
+++ csharp/A001/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace A001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = TwoSum(new int[] { 2,7,11,15 },9);
+            //Console.WriteLine(a.Length);
+            Console.WriteLine(a[0].ToString()+a[1].ToString());
+        }
+
+        public static int[] TwoSum(int[] nums,int target)
+        {
+            Dictionary<int,int> dict = new Dictionary<int,int>();
+
+            for(int i = 0;i<nums.Length;i++)
+            {
+                int require = target-nums[i];
+                if(dict.ContainsKey(require))
+                {
+                    return new int[] { i,dict[require] };
+                }
+                else
+                {
+                    dict[nums[i]]=i;
+                    //dict.Add(nums[i],i);
+                }
+            }
+
+            return null;
+        }
+    }
+}
---
+++ csharp/A002/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace A002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //ListNode<int> a1 = new ListNode<int>(2);
+            //ListNode<int> a2 = new ListNode<int>(4);
+            //ListNode<int> a3 = new ListNode<int>(3);
+            //a1.next=a2;
+            //a2.next=a3;
+
+            //ListNode<int> b1 = new ListNode<int>(5);
+            //ListNode<int> b2 = new ListNode<int>(6);
+            //ListNode<int> b3 = new ListNode<int>(4);
+            //b1.next=b2;
+            //b2.next=b3;
+            ListNode<int> a1 = new ListNode<int>(9);
+            ListNode<int> a2 = new ListNode<int>(9);
+            ListNode<int> a3 = new ListNode<int>(9);
+            a1.next=a2;
+            a2.next=a3;
+
+            ListNode<int> b1 = new ListNode<int>(1);
+
+            ListNode<int> ln = AddTwoNumbers(a1,b1);
+            while(ln!=null){
+                Console.WriteLine(ln.value);
+                ln=ln.next;
+            }
+            Console.ReadKey();
+        }
+
+        public static ListNode<int> AddTwoNumbers(ListNode<int> l1,ListNode<int> l2)
+        {
+            ListNode<int> result= new ListNode<int>(0);
+            ListNode<int> current =null;
+            int overflow = 0;
+            while(l1!=null||l2!=null||overflow!=0)
+            {
+                var l1value = l1==null ? 0 : l1.value;
+                var l2value = l2==null ? 0 : l2.value;
+
+                int sum = l1value+l2value+overflow;
+
+                overflow=sum/10;
+                var newNode = new ListNode<int>(sum%10);
+                if(current==null){
+                    current=newNode;
+                    result=current;
+                }
+                else{
+                    current.next=newNode;
+                    current=newNode;
+                }
+                if(l1!=null) l1=l1.next;
+                if(l2!=null) l2=l2.next;
+            }
+
+            return result;
+        }
+
+        public class ListNode<T> {
+            public T value;
+            public ListNode<T> next=null;
+
+            public ListNode(T value)
+            {
+                this.value=value;
+            }
+        }
+    }
+
+}
